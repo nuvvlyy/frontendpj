08c6325e5f36e091a8e49b1554c3a93c72f91724
nutthiyapohn <nutnutnut.1998@gmail.com>
2020-03-10 14:24:03 +0700

    ีupdate

--- server.js
+++ server.js
@@ -1,37 +1,15 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-//
-// const app = express();
-//
-// // Serve only the static files form the dist directory
-// app.use(express.static('./dist/src'));
-//
-// app.get('/*', function(req,res) {
-//   res.sendFile(path.join(__dirname,'./src/index.html'));
-// });
-//
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 4200);
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-const port = process.env.PORT || 4200;
-const server = require('http').Server(app);
 
-app.use(express.static(__dirname, 'dist', {index: false}));
+const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/luckystonepj'));
 
-server.listen(port, function() {
-  console.log("App running on port " + port);
-})
-
-// PathLocationStrategy
-
-app.get('', function(req, res) {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname,'.dist/luckystonepj/index.html'));
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 4200);
