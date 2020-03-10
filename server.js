338fb0651eeec335884e79a591a0f5de872b5024
nutthiyapohn <nutnutnut.1998@gmail.com>
2020-03-10 12:43:08 +0700

    update to fix bug when deploy heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/luckystonepj'));
+app.use(express.static('./dist/src'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'./dist/luckystonepj/index.html'));
+  res.sendFile(path.join(__dirname,'./src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
