c8ccc36a0ac9cd3ea7c8576a096fa3b1827353f5
nutthiyapohn <nutnutnut.1998@gmail.com>
2020-03-10 12:28:53 +0700

    update to fix bug when deploy heroku  add dot(.)

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/luckystonepj'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'/dist/luckystonepj/index.html'));
+  res.sendFile(path.join(__dirname,'./dist/luckystonepj/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
