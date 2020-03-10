1577b2637f9443feed19af67fc26e82753f50b6f
nutthiyapohn <nutnutnut.1998@gmail.com>
2020-03-10 11:14:02 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-
-
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname,  '/dist/luckystonepj'));
+app.use(express.static('./dist/luckystonepj'));
 
-app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname, '/dist/luckystonepj/index.html'));
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname,'/dist/luckystonepj/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
