61cb295113b64f3abc50c61154991d3224b36da9
nutthiyapohn <nutnutnut.1998@gmail.com>
2020-04-13 20:21:57 +0700

    update to braceletPattern

--- src/app/design_bracelet/step1/step1.component.ts
+++ src/app/design_bracelet/step1/step1.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {ApiService} from '../../service/api.service';
+import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-step1',
@@ -16,7 +19,20 @@ export class Step1Component implements OnInit {
   @Output() stoneShape = new EventEmitter<String>();
   @Output() isStep2= new EventEmitter<boolean>();
 
+  modal ; NgbModalRef
+  constructor(
+    private modalService: NgbModal,
+    private api: ApiService,) {
+  }
+  uid;
+  braceletPattern
   ngOnInit() {
+    if (localStorage.getItem('uid')) {
+      this.uid = localStorage.getItem('uid');
+      this.getBraceletPattern(this.uid);
+
+    }
+
   }
   submit(e) {
     if(this._wristSize && this._stoneSize && this._stoneShape){
@@ -29,5 +45,24 @@ export class Step1Component implements OnInit {
       this.isError = true;
     }
 
+  }
+  getBraceletPattern(id){
+    let params = new HttpParams();
+    params.append("user_id", id);
+    this.api.getBraceletPattern(params).subscribe(value => {
+      // @ts-ignore
+      this.braceletPattern = value.result.results
+    })
+  }
+  open(content) {
+    this.modal = this.modalService.open(content)
+  }
+  selectBraceletPattern(pattern){
+    this.modal = this.modalService.dismissAll();
+    localStorage.setItem('pattern',JSON.stringify(pattern));
+    this.wristSize.emit(pattern.wristSize);
+    this.stoneSize.emit(pattern.stoneSize);
+    this.stoneShape.emit(this._stoneShape);
+    this.isStep2.emit(true);
   }
 }
