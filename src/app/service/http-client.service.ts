6ee22fa502c5880ad2738af38d592b7af89e186f
nutthiyapohn <nutnutnut.1998@gmail.com>
2020-05-02 01:36:24 +0700

    fix bug

--- src/app/service/http-client.service.ts
+++ src/app/service/http-client.service.ts
@@ -16,8 +16,12 @@ export class HttpClientService {
     this.httpHeaders.append('Content-Type', 'text/html');
     this.httpHeaders.append('Accept', 'application/json');
     this.httpHeaders.append('Accept', 'text/plain');
-    this.httpHeaders.append('Access-Control-Allow-Origin', 'https://luckystoneadmin-api.herokuapp.com');
+    // this.httpHeaders.append('Access-Control-Allow-Origin', 'https://luckystoneadmin-api.herokuapp.com');
     this.httpHeaders.append('Access-Control-Allow-Credentials', 'true');
+    this.httpHeaders.append('Access-Control-Allow-Origin', '*');
+    this.httpHeaders.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    this.httpHeaders.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
     this.httpHeaders.append('X-CSRFTOKEN', 'gZucgSZPYReQomGO24vZKCVYxrI6l2XwHRJmgC9b1yFlzVAEpcVoKrhtJ7RWB72D');
     // this.httpHeaders.append("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
     // this.httpHeaders.append("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
@@ -42,6 +46,11 @@ export class HttpClientService {
     headers.append('Content-Type', 'multipart/form-data');
     headers.append('Content-Type', 'text/html');
     headers.append('Access-Control-Allow-Headers', 'Content-Type');
+    headers.append('Access-Control-Allow-Origin', 'https://luckystoneadmin-api.herokuapp.com');
+    headers.append('Access-Control-Allow-Credentials', 'true');
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     headers.append('Access-Control-Allow-Origin', 'https://luckystoneadmin-api.herokuapp.com');
     return await this.to(this.http.post(url,data,{headers: headers}))
   }
