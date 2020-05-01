21535e713aa9db76067f2506c096235407f9fe34
nutthiyapohn <nutnutnut.1998@gmail.com>
2020-05-02 00:27:50 +0700

    fix bug

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // });
 app.use(express.static(path.join(__dirname, '/dist/frontend/')));
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/frontend/"));
+  res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
