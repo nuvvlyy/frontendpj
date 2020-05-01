e7e51af73bd39b1e0988d77d254b4aeb8b4a4344
nutthiyapohn <nutnutnut.1998@gmail.com>
2020-05-01 23:52:29 +0700

    fix bug
    fix Uncaught SyntaxError: Unexpected token <

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/frontend'));
-
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'/dist/frontend/index.html'));
+// app.use(express.static('/dist/frontend'));
+//
+// app.get('/*', function(req,res) {
+//   res.sendFile(path.join(__dirname,'/dist/frontend/index.html'));
+// });
+app.use(express.static(path.join(__dirname, '/dist/frontend/')));
+app.listen(port);
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/frontend/"));
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
 // "postinstall": "ng build --prod --base-href frontend --build-optimizer --vendor-chunk --named-chunks false --output-hashing all
